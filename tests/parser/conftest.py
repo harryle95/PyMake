c7758b86b0dd7c769d4ca9e2dacad6cdfdd63701
hoangson <lehoangsonsg@gmail.com>
2023-09-10 10:40:02 +0930

    test: reorganise test cases

--- tests/parser/conftest.py
+++ tests/parser/conftest.py
@@ -1,144 +1,196 @@
 import pytest
+import yaml
 
+from PyMake.builder.var.section import VarSection
 from PyMake.parser.parser import VarParser
 
 
+########################################### PARSER 1 TEST ITEMS ########################################################
 @pytest.fixture(scope="function")
-def parser_1() -> VarParser:
-    return VarParser(
-        variables={
-            "var1": "basic",
-            "var2": "basic",
-            "var3": "basic",
-        },
-        positional={0: "var1", 1: "var2", 2: "var3"},
-        default={"var1": 10, "var2": 20, "var3": 20},
-        required=[]
-    )
-
-
-@pytest.fixture(scope="function")
-def parser_2() -> VarParser:
-    return VarParser(
-        variables={
-            "var1": "basic",
-            "var2": "basic",
-        },
-        positional={0: "var1", 1: "var2"},
-        default={"var1": 10, "var2": 20},
-        required=[]
-    )
+def parser1() -> VarParser:
+    data = """
+    basic:
+        var1: 100
+        var2: 100
+        var3: 100
+    """
+    data = yaml.safe_load(data)
+    section = VarSection(data)
+    yield section.build()
 
 
 @pytest.fixture(scope="function")
-def parser_3() -> VarParser:
-    return VarParser(
-        variables={
-            "var1": "basic",
-            "var2": "basic",
-            "var3": "basic"
-        },
-        positional={0: "var1", 1: "var2", 2: "var3"},
-        default={},
-        required=["var1", "var2", "var3"]
-    )
+def valid_parser1_input1():
+    return {
+        "args": "1 2 3",
+        "namespace": {"var1": "1", "var2": "2", "var3": "3"}
+    }
 
 
 @pytest.fixture(scope="function")
-def input_1():
-    return "--var1 100 --var2 100 --var3 100"
+def valid_parser1_input2():
+    return {
+        "args": "1 2 --var3 3",
+        "namespace": {"var1": "1", "var2": "2", "var3": "3"}
+    }
 
 
 @pytest.fixture(scope="function")
-def input_2():
-    return "100 200 --var3 300"
+def valid_parser1_input3():
+    return {
+        "args": "1 --var3 3 --var2 2",
+        "namespace": {"var1": "1", "var2": "2", "var3": "3"}
+    }
 
 
 @pytest.fixture(scope="function")
-def input_3():
-    return "100 --var2 0.5 --var3 300"
+def valid_parser1_input4():
+    return {
+        "args": "--var1 1 --var2 2 --var3 3",
+        "namespace": {"var1": "1", "var2": "2", "var3": "3"}
+    }
 
 
 @pytest.fixture(scope="function")
-def input_4():
-    return "100 --var3 0.5 --var2 300"
+def valid_parser1_input5():
+    return {
+        "args": "--var1 1 --var3 3 --var2 2",
+        "namespace": {"var1": "1", "var2": "2", "var3": "3"}
+    }
 
 
 @pytest.fixture(scope="function")
-def input_5():
-    return "100 ABC --var3 300"
+def valid_parser1_input6():
+    return {
+        "args": "--var1 1 --var2 2",
+        "namespace": {"var1": "1", "var2": "2", "var3": "100"}
+    }
 
 
 @pytest.fixture(scope="function")
-def input_6():
-    return "100 200"
+def valid_parser1_input7():
+    return {
+        "args": "--var1 1 --var3 3",
+        "namespace": {"var1": "1", "var2": "100", "var3": "3"}
+    }
 
 
 @pytest.fixture(scope="function")
-def input_7():
-    return "100 --var2 ASD"
+def valid_parser1_input8():
+    return {
+        "args": "--var1 1",
+        "namespace": {"var1": "1", "var2": "100", "var3": "100"}
+    }
 
 
 @pytest.fixture(scope="function")
-def input_8():
-    return "--var2 100 --var1 ASD"
-
-@pytest.fixture(scope="function")
-def input_9():
-    return "--var1 100 1000 --var2 10"
-
+def valid_parser1_input9():
+    return {
+        "args": "--var3 3",
+        "namespace": {"var1": "100", "var2": "100", "var3": "3"}
+    }
 
 
 @pytest.fixture(scope="function")
-def parser1_input1_output():
-    return {"var1": '100', "var2": '100', "var3": '100'}
+def valid_parser1_input10():
+    return {
+        "args": "",
+        "namespace": {"var1": "100", "var2": "100", "var3": "100"}
+    }
 
 
 @pytest.fixture(scope="function")
-def parser1_input2_output():
-    return {"var1": '100', "var2": '200', "var3": '300'}
+def valid_parser1_input11():
+    return {
+        "args": "1",
+        "namespace": {"var1": "1", "var2": "100", "var3": "100"}
+    }
 
 
 @pytest.fixture(scope="function")
-def parser1_input3_output():
-    return {"var1": '100', "var2": '0.5', "var3": '300'}
+def valid_parser1_input12():
+    return {
+        "args": "1 2",
+        "namespace": {"var1": "1", "var2": "2", "var3": "100"}
+    }
 
 
 @pytest.fixture(scope="function")
-def parser1_input4_output():
-    return {"var1": '100', "var2": '300', "var3": '0.5'}
+def invalid_parser1_undefined_variable_1():
+    return {
+        "args": "--var 1 2",
+    }
 
 
 @pytest.fixture(scope="function")
-def parser1_input5_output():
-    return {"var1": '100', "var2": 'ABC', "var3": '300'}
+def invalid_parser1_undefined_variable_2():
+    return {
+        "args": "--var1 2 --var4",
+    }
 
 
 @pytest.fixture(scope="function")
-def parser1_input6_output():
-    return {"var1": '100', "var2": '200', "var3": '20'}
+def invalid_parser1_undefined_variable_3():
+    return {
+        "args": "--flag1",
+    }
 
 
 @pytest.fixture(scope="function")
-def parser1_input7_output():
-    return {"var1": '100', "var2": 'ASD', "var3": '20'}
+def invalid_parser1_undefined_variable_4():
+    return {
+        "args": "--sequence1 1 2 3 4",
+    }
 
 
 @pytest.fixture(scope="function")
-def parser1_input8_output():
-    return {"var1": 'ASD', "var2": '100', "var3": '20'}
+def invalid_parser1_invalid_value_1():
+    return {
+        "args": "--var1 2 3",
+    }
 
 
 @pytest.fixture(scope="function")
-def parser2_input6_output():
-    return {"var1": '100', "var2": '200'}
+def invalid_parser1_invalid_value_2():
+    return {
+        "args": "1 --var2 2 3",
+    }
 
 
 @pytest.fixture(scope="function")
-def parser2_input7_output():
-    return {"var1": '100', "var2": 'ASD'}
+def invalid_parser1_variable_redefinition_1():
+    return {
+        "args": "1 --var1 2",
+    }
 
 
 @pytest.fixture(scope="function")
-def parser2_input8_output():
-    return {"var1": 'ASD', "var2": '100'}
+def invalid_parser1_variable_redefinition_2():
+    return {
+        "args": "--var1 1 --var1 2",
+    }
+
+
+########################################### PARSER 2 TEST ITEMS ########################################################
+@pytest.fixture(scope="function")
+def parser2() -> VarParser:
+    data = """
+    basic:
+        var1: 100
+        var2: REQUIRED
+        var3: 100
+    flag: 
+        flag1: "-A"
+    sequence:
+        seq1: [1,2,3,4,5]
+    """
+    data = yaml.safe_load(data)
+    section = VarSection(data)
+    yield section.build()
+
+@pytest.fixture(scope="function")
+def valid_parser2_input1():
+    return {
+        "args": "1 2 3",
+        "namespace": {"var1": "1", "var2": "2", "var3": "3", "seq1": "1 2 3 4 5"}
+    }
--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -1,70 +1,84 @@
 import pytest
 
-from PyMake.exceptions import MissingRequiredVariableError, UndefinedVariableError, InvalidValueError
+from PyMake.exceptions import InvalidValueError, UndefinedVariableError, \
+    VariableRedefinitionError
 
 
 @pytest.mark.parametrize(
-    "parser, args, output", [
-        ("parser_1", "input_1", "parser1_input1_output"),
-        ("parser_1", "input_2", "parser1_input2_output"),
-        ("parser_1", "input_3", "parser1_input3_output"),
-        ("parser_1", "input_4", "parser1_input4_output"),
-        ("parser_1", "input_5", "parser1_input5_output"),
-        ("parser_1", "input_6", "parser1_input6_output"),
-        ("parser_1", "input_7", "parser1_input7_output"),
-        ("parser_1", "input_8", "parser1_input8_output"),
-        ("parser_2", "input_6", "parser2_input6_output"),
-        ("parser_2", "input_7", "parser2_input7_output"),
-        ("parser_2", "input_8", "parser2_input8_output"),
+    "parser, valid_input", [
+        # ("parser1", "valid_parser1_input1"),
+        # ("parser1", "valid_parser1_input2"),
+        # ("parser1", "valid_parser1_input3"),
+        # ("parser1", "valid_parser1_input4"),
+        # ("parser1", "valid_parser1_input5"),
+        # ("parser1", "valid_parser1_input6"),
+        # ("parser1", "valid_parser1_input7"),
+        # ("parser1", "valid_parser1_input8"),
+        # ("parser1", "valid_parser1_input9"),
+        # ("parser1", "valid_parser1_input10"),
+        # ("parser1", "valid_parser1_input11"),
+        # ("parser1", "valid_parser1_input12"),
+        ("parser2", "valid_parser2_input1"),
     ]
 )
-def test_parser_valid(parser, args, output, request):
+def test_parser_valid(parser, valid_input, request):
     parser = request.getfixturevalue(parser)
-    arg = request.getfixturevalue(args)
-    output = request.getfixturevalue(output)
-    parser.parse(arg)
-    assert parser.namespace == output
+    valid_input = request.getfixturevalue(valid_input)
+    parser.parse(valid_input['args'])
+    assert parser.namespace == valid_input['namespace']
 
 
 @pytest.mark.parametrize(
-    "parser, args", [
-        ("parser_2", "input_1"),
-        ("parser_2", "input_2"),
-        ("parser_2", "input_3"),
-        ("parser_2", "input_4"),
-        ("parser_2", "input_5"),
+    "parser, invalid_input", [
+        ("parser1", "invalid_parser1_undefined_variable_1"),
+        ("parser1", "invalid_parser1_undefined_variable_2"),
+        ("parser1", "invalid_parser1_undefined_variable_3"),
+        ("parser1", "invalid_parser1_undefined_variable_4"),
     ]
 )
-def test_parser_undefined_var(parser, args, request):
+def test_parser_undefined_var(parser, invalid_input, request):
     parser = request.getfixturevalue(parser)
-    arg = request.getfixturevalue(args)
+    args = request.getfixturevalue(invalid_input)['args']
     with pytest.raises(UndefinedVariableError):
-        parser.parse(arg)
+        parser.parse(args)
 
 
 @pytest.mark.parametrize(
-    "parser, args", [
-        ("parser_3", "input_6"),
-        ("parser_3", "input_7"),
-        ("parser_3", "input_8"),
+    "parser, invalid_input", [
+        ("parser1", "invalid_parser1_invalid_value_1"),
+        ("parser1", "invalid_parser1_invalid_value_2"),
     ]
 )
-def test_parser_missing_required(parser, args, request):
+def test_parser_invalid_value(parser, invalid_input, request):
     parser = request.getfixturevalue(parser)
-    arg = request.getfixturevalue(args)
-    with pytest.raises(MissingRequiredVariableError):
-        parser.parse(arg)
-
-
-@pytest.mark.parametrize(
-    "parser, args", [
-        ("parser_1", "input_9"),
-        ("parser_2", "input_9"),
-        ("parser_3", "input_9"),
-    ]
-)
-def test_parser_invalid_value(parser, args, request):
-    parser = request.getfixturevalue(parser)
-    arg = request.getfixturevalue(args)
+    args = request.getfixturevalue(invalid_input)['args']
     with pytest.raises(InvalidValueError):
-        parser.parse(arg)
+        parser.parse(args)
+
+
+@pytest.mark.parametrize(
+    "parser, invalid_input", [
+        ("parser1", "invalid_parser1_variable_redefinition_1"),
+        ("parser1", "invalid_parser1_variable_redefinition_2"),
+    ]
+)
+def test_parser_variable_redefinition(parser, invalid_input, request):
+    parser = request.getfixturevalue(parser)
+    args = request.getfixturevalue(invalid_input)['args']
+    with pytest.raises(VariableRedefinitionError):
+        parser.parse(args)
+
+
+@pytest.mark.parametrize(
+    "parser, invalid_input", [
+        ("parser1", "invalid_parser1_undefined_variable_1"),
+        ("parser1", "invalid_parser1_undefined_variable_2"),
+        ("parser1", "invalid_parser1_undefined_variable_3"),
+        ("parser1", "invalid_parser1_undefined_variable_4"),
+    ]
+)
+def test_parser_variable_redefinition(parser, invalid_input, request):
+    parser = request.getfixturevalue(parser)
+    args = request.getfixturevalue(invalid_input)['args']
+    with pytest.raises(UndefinedVariableError):
+        parser.parse(args)
