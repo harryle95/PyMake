1c5c3f9d3cbf1a4e0830b45b8fac59bd8714e7fe
hoangson <lehoangsonsg@gmail.com>
2023-09-14 14:05:53 +0930

    feat: add envs property

--- PyMake/console/builder/env_builder.py
+++ PyMake/console/builder/env_builder.py
@@ -13,17 +13,23 @@ class EnvModel(BaseModel):
     data: EnvType
     _default: dict[str, str] = {}
     _reference: dict[str, str] = {}
+    _envs: dict[str, str] = {}
 
     def build(self):
         pattern = r"\$\((.*?)\)"
         if self.data:
+            self._envs = self.data
             for key, value in self.data.items():
                 value = str(value).strip()
                 match = re.findall(pattern, value)
                 if match:
-                    self._reference[key] = match[0]
+                    self._reference[match[0]] = key
                 else:
-                    self._default[key] = value
+                    self._default[value] = key
+
+    @property
+    def envs(self) -> dict[str, str]:
+        return self._envs
 
     @property
     def default(self) -> dict[str, str]:
