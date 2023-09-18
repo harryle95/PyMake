a17484f427f6747730b9a2ce9cbb991a8ea5fae5
hoangson <lehoangsonsg@gmail.com>
2023-09-18 23:29:33 +0930

    feat: add methods to parse scripts with shebang and dictionary

--- PyMake/console/builder/cmd_builder.py
+++ PyMake/console/builder/cmd_builder.py
@@ -1,35 +1,79 @@
+import dataclasses
 import re
+from typing import Literal
 
 from PyMake.console.builder.abc_builder import ListDefaultModel, PATTERN
 from PyMake.decorators import validate_raise_exception
 from PyMake.exceptions import InvalidCmdType
 
-CmdType = str | list[str] | None
+Lang = Literal["python", "bash"]
+
+CmdEntry = str | dict[Lang, str | list[str]]
+CmdType = CmdEntry | list[CmdEntry] | None
+
+
+@dataclasses.dataclass
+class Command:
+    command: str
+    executable: str | None
+
+
+def get_shebang(command: str) -> str | None:
+    command = command.strip()
+    pattern = "^#!\s*(.+)"
+    match = re.findall(pattern, command)
+    if match:
+        return match[0]
+
+
+def parse_string(entry: str, exe: str | None = None) -> Command:
+    executable = get_shebang(entry)
+    if exe is None and executable is None:
+        return Command(entry, None)
+    if executable:
+        return Command(entry, executable)
+    if exe == "python":
+        return Command(entry, "python")
+    if exe == "bash":
+        return Command(entry, "bash")
+    raise ValueError(f"Unsupported executable: {exe}")
+
+
+def parse_command(entry: CmdType) -> list[Command]:
+    if not isinstance(entry, list):
+        entry = [entry]
+    commands = []
+    for item in entry:
+        if isinstance(item, str):
+            commands.append(parse_string(item))
+        if isinstance(item, dict):
+            for key, value in item.items():
+                if isinstance(value, str):
+                    commands.append(parse_string(value, key))
+                else:
+                    commands.extend(
+                        [parse_string(sub_value, key) for sub_value in value]
+                    )
+    return commands
 
 
 @validate_raise_exception(InvalidCmdType)
 class CmdModel(ListDefaultModel):
     data: CmdType
     _reference: list[str] = []
-    _commands: list[str] = []
+    _commands: list[Command] = []
 
     def build(self):
         if self.data:
-            if isinstance(self.data, str):
-                data = [self.data]
-            else:
-                data = self.data
-            self._extract_reference(data)
+            self._commands = parse_command(self.data)
+            self._extract_reference(self._commands)
         else:
             raise InvalidCmdType("cmd cannot be empty.")
 
-    def _extract_reference(self, data: list[str]) -> None:
+    def _extract_reference(self, data: list[Command]) -> None:
         matches = set()
-        cmds = []
         for cmd in data:
-            cmds.append(cmd)
-            matches.update(re.findall(PATTERN, cmd))
-        self._commands = list(cmds)
+            matches.update(re.findall(PATTERN, cmd.command))
         self._reference = list(matches)
 
     @property
@@ -38,4 +82,8 @@ class CmdModel(ListDefaultModel):
 
     @property
     def commands(self) -> list[str]:
+        return [item.command for item in self._commands]
+
+    @property
+    def executing_commands(self) -> list[Command]:
         return self._commands
